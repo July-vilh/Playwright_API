26a8a35886bcd1dc7990326d38d4596555332c30
July <106986983+July-vilh@users.noreply.github.com>
2024-10-14 16:58:09 +0300

    Create constants_for_theory_file.js

---
+++ Practice/POST_API/constants_for_theory_file.js
@@ -0,0 +1,8 @@
+// constants
+const API_BASE_URL = 'http://restful-booker.herokuapp.com/booking'
+const POST_URL = 'https://reqres.in/api/users'
+
+module.exports = { // object for export data
+    API_BASE_URL,
+    POST_URL
+};
